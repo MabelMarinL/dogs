f2cc9447be4f9d256573c6c3accb7a23078e945e
Mabel Marin <mabel09mar@gmail.com>
2023-10-02 17:27:05 -0500

    api

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const { PORT } = process.env;
 
-const PORT = 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
